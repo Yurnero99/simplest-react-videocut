110404de0b5f4b52c97e06afd4b50b750cf9c476
Robert Gevorgyan <robert1999.g@gmail.com>
2017-02-08 10:14:06 +0400

    Add video component, loader

--- lib/main.jsx
+++ lib/main.jsx
@@ -6,6 +6,20 @@ import ReactDOM from 'react-dom'
 class VideoCutter extends Component {
   constructor() {
     super()
+    this.state = {videoBlob: ''}
+    this.loadVideo = this.loadVideo.bind(this)
+  }
+
+  loadVideo(e) {
+    const currentFile = e.target.files[0]
+    var video = document.getElementById('video')
+    let blob = ''
+    if (e.target.files && currentFile) {
+      blob = URL.createObjectURL(currentFile);
+      console.log(blob)
+      this.setState({videoBlob: blob})
+    }
+    video.load()
   }
 
   componentDidMount(){
@@ -13,10 +27,13 @@ class VideoCutter extends Component {
   }
 
   render() {
-    let styles = {color: 'red'}
     return (
       <div>
         <p>Hello World!</p>
+        <input type="file" accept="video/mp4,video/x-m4v,video/*" name="file" onChange={this.loadVideo} />
+        <video id="video" controls preload="auto" width="640" height="264">
+          <source src={this.state.videoBlob} type="video/mp4" />
+        </video>
       </div>
     )
   }
