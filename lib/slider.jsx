fbca8f1aa4dccee652c8a1c2751621fa3ffe5dcb
Robert Gevorgyan <robert1999.g@gmail.com>
2017-02-15 10:34:21 +0400

    Fix some bugs

--- lib/slider.jsx
+++ lib/slider.jsx
@@ -10,22 +10,22 @@ export default
 class RangeSlider extends Component {
   constructor() {
     super()
-    this.state = { x: 0, start: 0, step: 0, pos: 0 }
+    this.state = { x: 0, start: 0, end: 0, step: 0, pos: 0, width: 0 }
     this.handleDrag = this.handleDrag.bind(this)
     this.slideChanged = this.slideChanged.bind(this)
   }
 
   slideChanged(e) {
-    const {max} = this.props
-    const start = e[0] * 100 / max
-    this.setState({start: start})
+    const width = (e[1] - e[0]) * 100 / this.props.max
+    this.setState({start: e[0], end: e[1], width: width})
   }
 
   handleDrag(e, ui) {
-    const {x, step} = this.state
+    const {x, step, start, end} = this.state
     const newX = x + ui.deltaX
     const pos = Math.round(newX / step)
-    this.setState({x: newX, pos: pos})
+    const width = end - start
+    this.setState({x: newX, start: pos, end: pos+width})
   }
 
   componentDidMount() {
@@ -37,18 +37,18 @@ class RangeSlider extends Component {
   }
 
   render() {
-    const {min, max, start, end} = this.props
-    const {x, step, pos} = this.state
+    const {min, max} = this.props
+    const {x, step, pos, start, end} = this.state
     console.log(pos)
     return (
       <div className="slider-container">
         <img className="timeline" src="http://www.myhimalayas.com/namtso/image/large/panorama_lalungla_wide_shishapangma_small.jpg" />
         <div className="slider-wrapper" id="slider">
-          <Range value={[pos, pos+1]} defaultValue={[1, 2]} min={min} max={max} onChange={this.slideChanged} />
+          <Range value={[start, end]} defaultValue={[1, 2]} min={min} max={max} onChange={this.slideChanged} />
           <Draggable grid={[step, 0]} bounds="parent" onDrag={this.handleDrag} axis="x">
             <div
               className="drag-body"
-              style={{width: `${100/this.state.limit}%`}}>
+              style={{width: `${this.state.width}%`}}>
             </div>
           </Draggable>
         </div>
